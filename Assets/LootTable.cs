115bee94ec69b4e13618938e5a46450721d3700e
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-11-22 11:13:21 -0400

    Fixed formatting

--- Assets/LootTable.cs
+++ Assets/LootTable.cs
@@ -8,36 +8,6 @@ public class LootTable : MonoBehaviour
 {
     [SerializeField] private List<string> ChestTypes = new List <string>();
     [SerializeField] private List<string> DropTypes = new List<string>();
-    //[Header("Wood Chest")]
-    //[SerializeField] private List<int> wCommon;
-    //[SerializeField] private List<int> wUncommon;
-    //[SerializeField] private List<int> wRare;
-    //[SerializeField] private List<int> wEpic;
-    //[SerializeField] private List<int> wLegendary;
-    //[Header("Bronze Chest")]
-    //[SerializeField] private List<int> bCommon;
-    //[SerializeField] private List<int> bUncommon;
-    //[SerializeField] private List<int> bRare;
-    //[SerializeField] private List<int> bEpic;
-    //[SerializeField] private List<int> bLegendary;
-    //[Header("Silver Chest")]
-    //[SerializeField] private List<int> sCommon;
-    //[SerializeField] private List<int> sUncommon;
-    //[SerializeField] private List<int> sRare;
-    //[SerializeField] private List<int> sEpic;
-    //[SerializeField] private List<int> sLegendary;
-    //[Header("Gold Chest")]
-    //[SerializeField] private List<int> gCommon;
-    //[SerializeField] private List<int> gUncommon;
-    //[SerializeField] private List<int> gRare;
-    //[SerializeField] private List<int> gEpic;
-    //[SerializeField] private List<int> gLegendary;
-    //[Header("Platinum Chest")]
-    //[SerializeField] private List<int> pCommon;
-    //[SerializeField] private List<int> pUncommon;
-    //[SerializeField] private List<int> pRare;
-    //[SerializeField] private List<int> pEpic;
-    //[SerializeField] private List<int> pLegendary;
 
     // Start is called before the first frame update
     void Start()
@@ -94,132 +64,127 @@ public class LootTable : MonoBehaviour
         int pEpic = pRare + 15;
         int pLegendary = pEpic + 10;
 
-        if (Input.GetKeyUp(KeyCode.L))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             int test = Random.Range(0, 379);
             if (test <= wood)
             {
-                Debug.Log("Found: " + ChestTypes[0] + " Chest!");
                 int woodType = Random.Range(0, 250);
                 if (woodType <= wCommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[0] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[0] + " Chest! \nChest contains: One " + DropTypes[0] + " Item!");
                 }
                 else if (woodType > wCommon && woodType <= wUncommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[1] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[0] + " Chest! \nChest contains: One " + DropTypes[1] + " Item!");
                 }
                 else if (woodType > wUncommon && woodType <= wRare)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[2] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[0] + " Chest! \nChest contains: One " + DropTypes[2] + " Item!");
                 }
                 else if (woodType > wRare && woodType <= wEpic)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[3] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[0] + " Chest! \nChest contains: One " + DropTypes[3] + " Item!");
                 }
                 else if (woodType > wEpic && woodType <= wLegendary)
                 {
-                    Debug.Log("Chest contains: One  " + DropTypes[4] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[0] + " Chest! \nChest contains: One  " + DropTypes[4] + " Item!");
                 }
             }
             else if (test > wood && test <= bronze)
             {
-                Debug.Log("Found: " + ChestTypes[1] + " Chest!");
                 int bronzeType = Random.Range(0, 250);
                 if (bronzeType <= bCommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[0] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[1] + " Chest! \nChest contains: One " + DropTypes[0] + " Item!");
                 }
                 else if (bronzeType > bCommon && bronzeType <= bUncommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[1] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[1] + " Chest! \nChest contains: One " + DropTypes[1] + " Item!");
                 }
                 else if (bronzeType > bUncommon && bronzeType <= bRare)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[2] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[1] + " Chest! \nChest contains: One " + DropTypes[2] + " Item!");
                 }
                 else if (bronzeType > bRare && bronzeType <= bEpic)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[3] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[1] + " Chest! \nChest contains: One " + DropTypes[3] + " Item!");
                 }
                 else if (bronzeType > bEpic && bronzeType <= bLegendary)
                 {
-                    Debug.Log("Chest contains: One  " + DropTypes[4] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[1] + " Chest! \nChest contains: One  " + DropTypes[4] + " Item!");
                 }
             }
             else if (test > bronze && test <= silver)
             {
-                Debug.Log("Found: " + ChestTypes[2] + " Chest!");
                 int silverType = Random.Range(0, 164);
                 if (silverType <= sCommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[0] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[2] + " Chest! \nChest contains: One " + DropTypes[0] + " Item!");
                 }
                 else if (silverType > sCommon && silverType <= sUncommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[1] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[2] + " Chest! \nChest contains: One " + DropTypes[1] + " Item!");
                 }
                 else if (silverType > sUncommon && silverType <= sRare)
                 {
-                    Debug.Log("Chest contains: One  " + DropTypes[2] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[2] + " Chest! \nChest contains: One  " + DropTypes[2] + " Item!");
                 }
                 else if (silverType > sRare && silverType <= sEpic)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[3] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[2] + " Chest! \nChest contains: One " + DropTypes[3] + " Item!");
                 }
                 else if (silverType > sEpic && silverType <= sLegendary)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[4] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[2] + " Chest! \nChest contains: One " + DropTypes[4] + " Item!");
                 }
             }
             else if (test > silver && test <= gold)
             {
-                Debug.Log("Found: " + ChestTypes[3] + " Chest!");
                 int goldType = Random.Range(0, 90);
                 if (goldType <= gCommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[0] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[3] + " Chest! \nChest contains: One " + DropTypes[0] + " Item!");
                 }
                 else if (goldType > gCommon && goldType <= gUncommon)
                 {
-                    Debug.Log("Chest contains: One  " + DropTypes[1] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[3] + " Chest! \nChest contains: One  " + DropTypes[1] + " Item!");
                 }
                 else if (goldType > gUncommon && goldType <= gRare)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[2] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[3] + " Chest! \nChest contains: One " + DropTypes[2] + " Item!");
                 }
                 else if (goldType > gRare && goldType <= gEpic)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[3] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[3] + " Chest! \nChest contains: One " + DropTypes[3] + " Item!");
                 }
                 else if (goldType > gEpic && goldType <= gLegendary)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[4] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[3] + " Chest! \nChest contains: One " + DropTypes[4] + " Item!");
                 }
             }
             else if (test > gold && test <= platinum)
             {
-                Debug.Log("Found: " + ChestTypes[4] + " Chest!");
                 int platinumType = Random.Range(0, 64);
                 if (platinumType <= pCommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[0] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[4] + " Chest! \nChest contains: One " + DropTypes[0] + " Item!");
                 }
                 else if (platinumType > pCommon && platinumType <= pUncommon)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[1] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[4] + " Chest! \nChest contains: One " + DropTypes[1] + " Item!");
                 }
                 else if (platinumType > pUncommon && platinumType <= pRare)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[2] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[4] + " Chest! \nChest contains: One " + DropTypes[2] + " Item!");
                 }
                 else if (platinumType > pRare && platinumType <= pEpic)
                 {
-                    Debug.Log("Chest contains: One  " + DropTypes[3] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[4] + " Chest! \nChest contains: One  " + DropTypes[3] + " Item!");
                 }
                 else if (platinumType > pEpic && platinumType <= pLegendary)
                 {
-                    Debug.Log("Chest contains: One " + DropTypes[4] + " Item!");
+                    Debug.Log("Found: " + ChestTypes[4] + " Chest! \nChest contains: One " + DropTypes[4] + " Item!");
                 }
             }
         }
